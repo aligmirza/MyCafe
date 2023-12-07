40b55ed68d7489d205f986765e4ad6db9777b093
MyCafe <aligmirza1234@gmail.com>
2023-12-07 13:24:41 +0500

    Muhammad Ali

--- src/main/java/com/progmeleon/mycafe/ui/Menu.java
+++ src/main/java/com/progmeleon/mycafe/ui/Menu.java
@@ -6,6 +6,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -23,19 +24,51 @@ import static com.progmeleon.mycafe.ui.Components.primaryStage;
 
 public class Menu {
     private static ObservableList<Item> itemList = FXCollections.observableArrayList();
+    private static ObservableList<CartItem> cartItemList = FXCollections.observableArrayList();
+
+    // Create a TableView to display selected items with quantities
+    private static TableView<CartItem> tableView = new TableView<>();
+
+    // Create a label to display the total bill
+    private static Label totalLabel = new Label("Total: PKR 0.0");
 
     public static void showMenuItems() {
-        HBox manageItemsMenu = new HBox(10);
+        HBox mainLayout = new HBox(20);
+
+        // Create an HBox for menu items
+        VBox manageItemsMenu = new VBox(10);
         itemList.setAll(fetchItemsFromDatabase());
 
         for (Item item : itemList) {
             HBox menuItemHBox = new HBox();
-            HBox menuItemBox = menuItems(item.getItemName(), item.getItemPrice());
+            VBox menuItemBox = menuItems(item.getItemName(), item.getItemPrice(), item);
             menuItemHBox.getChildren().add(menuItemBox);
             manageItemsMenu.getChildren().add(menuItemHBox);
         }
 
-        ((BorderPane) primaryStage.getScene().getRoot()).setCenter(manageItemsMenu);
+        // Create VBox for TableView
+        VBox tableViewVBox = new VBox();
+        tableViewVBox.getChildren().add(new Label("Selected Items:"));
+
+        // Define columns for TableView
+        TableColumn<CartItem, String> itemNameColumn = new TableColumn<>("Item Name");
+        itemNameColumn.setCellValueFactory(new PropertyValueFactory<>("itemName"));
+
+        TableColumn<CartItem, Double> itemPriceColumn = new TableColumn<>("Item Price");
+        itemPriceColumn.setCellValueFactory(new PropertyValueFactory<>("itemPrice"));
+
+        TableColumn<CartItem, Integer> quantityColumn = new TableColumn<>("Quantity");
+        quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+
+        tableView.getColumns().addAll(itemNameColumn, itemPriceColumn, quantityColumn);
+        Button checkoutButton = new Button("Generate Bill");
+        checkoutButton.setOnAction(e -> printReceipt());
+
+        tableViewVBox.getChildren().addAll(tableView, checkoutButton,totalLabel);
+
+
+        mainLayout.getChildren().addAll(manageItemsMenu, tableViewVBox);
+        ((BorderPane) primaryStage.getScene().getRoot()).setCenter(mainLayout);
     }
 
     public static List<Item> fetchItemsFromDatabase() {
@@ -61,19 +94,83 @@ public class Menu {
         return items;
     }
 
-    public static HBox menuItems(String itemName, double itemPrice) {
-        HBox menuItemBox = new HBox(10);
+    public static VBox menuItems(String itemName, double itemPrice, Item item) {
+        VBox menuItemBox = new VBox(10);
         menuItemBox.setMaxWidth(200);
 
         menuItemBox.setStyle("-fx-border-color: red");
-        menuItemBox.setPadding(new Insets(20,10,10,20));
+        menuItemBox.setPadding(new Insets(20, 10, 10, 20));
         menuItemBox.setBackground(Background.fill(Color.WHITE));
         menuItemBox.setPadding(new Insets(10));
         Label itemNameLabel = new Label(itemName);
         Label itemPriceLabel = new Label("Price: PKR" + itemPrice);
-        Spinner<Integer> quantitySpinner = new Spinner<>(0,Integer.MAX_VALUE,0);
+        Spinner<Integer> quantitySpinner = new Spinner<>(0, Integer.MAX_VALUE, 0);
         Button addToCartButton = new Button("Add to Cart");
+
+        // Event handler to add the selected item to the TableView
+        addToCartButton.setOnAction(event -> {
+            int quantity = quantitySpinner.getValue();
+            if (quantity > 0) {
+                // Create a CartItem wrapper for the Item and its quantity
+                CartItem cartItem = new CartItem(item, quantity);
+                cartItemList.add(cartItem);
+                tableView.getItems().setAll(cartItemList);
+
+                // Update total label
+                updateTotalLabel();
+            }
+        });
+
         menuItemBox.getChildren().addAll(itemNameLabel, itemPriceLabel, quantitySpinner, addToCartButton);
         return menuItemBox;
+    }
+
+    // Simple wrapper class to hold Item and its quantity
+    public static class CartItem {
+        private Item item;
+        private int quantity;
+
+        public CartItem(Item item, int quantity) {
+            this.item = item;
+            this.quantity = quantity;
+        }
+
+        public String getItemName() {
+            return item.getItemName();
+        }
+
+        public double getItemPrice() {
+            return item.getItemPrice();
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+    }
+
+    // Update total label based on items in the cart
+    private static void updateTotalLabel() {
+        double total = cartItemList.stream()
+                .mapToDouble(cartItem -> cartItem.getItemPrice() * cartItem.getQuantity())
+                .sum();
+        totalLabel.setText("Total: PKR " + total);
+    }
+
+    // Print the receipt
+    public static void printReceipt() {
+        StringBuilder receipt = new StringBuilder("Receipt:\n");
+        for (CartItem cartItem : cartItemList) {
+            receipt.append(cartItem.getItemName())
+                    .append("\t")
+                    .append(cartItem.getQuantity())
+                    .append("\t")
+                    .append("PKR ")
+                    .append(cartItem.getItemPrice() * cartItem.getQuantity())
+                    .append("\n");
+        }
+        receipt.append("\nTotal: PKR ").append(totalLabel.getText().substring(8));
+
+        // You can modify this part to print or display the receipt as needed
+        System.out.println(receipt.toString());
     }
 }
